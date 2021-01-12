262fa1e41950a23f82f7fada06ca386be65582a8
Shehab Hosny <shehab2015@live.com>
2021-01-12 07:00:29 +0200

    Update DiffCppTest.cpp

--- test/DiffCppTest.cpp
+++ test/DiffCppTest.cpp
@@ -1,5 +1,60 @@
 #include <gtest/gtest.h>
+#include "../include/diff.h"
 
-TEST(githubActionsGtestExampleTest, firstTest) {
-  EXPECT_TRUE( 1 == 1 );
+// To pass virtual function arguments
+char* argvArray[3];
+char** argv = argvArray;
+
+// To Load Expected Results from the specified file
+vector<string> loadExpectedResults(string path)
+{
+
+    vector<string> lines;
+    fstream inputFile;
+    string singleLine;
+
+    inputFile.open(path);
+
+    // Check that the specified file can open successfully
+    if (inputFile.is_open())
+    {
+
+        while (getline(inputFile, singleLine))
+        {
+            lines.push_back(singleLine);
+        }
+        inputFile.close();
+    }
+    // Terminate the program
+    else
+    {
+        cout << "Can not open the specified directory - No such file or folder" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return lines;
+}
+
+TEST(TestCase1, CheckingSimpleDiff) {
+    argvArray[1] = (char*)"./TestCases/TestCase1/Input1.txt";
+    argvArray[2] = (char*)"./TestCases/TestCase1/Input2.txt";
+    
+    string lineToCheck;
+
+    DiffSession session(3, argv);
+
+    vector<string> linesFile1 = session.getLinesFromFile(argv[1]),
+                   linesFile2 = session.getLinesFromFile(argv[2]);
+
+    vector<string> expectedOutput = 
+        loadExpectedResults("./TestCases/TestCase1/ExpectedResults.txt");
+
+    vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
+
+    for (int i = 0; i < diff.size(); i++) {
+        lineToCheck = diff[i][0] + " " + diff[i][1];
+        EXPECT_EQ(lineToCheck, expectedOutput[i]);
+    }
+
+    EXPECT_TRUE(true);
 }
