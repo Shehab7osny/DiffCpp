e6bfdae2cd19ef98b296b2e944e142440b1cca89
Shehab Hosny <shehab2015@live.com>
2021-01-12 23:55:51 +0200

    Update diff.cpp

--- src/diff.cpp
+++ src/diff.cpp
@@ -10,9 +10,11 @@ int main(int argc, char** argv) {
         cout << "Insufficient number of file specifications" << endl;
         return 0;
     }
-
+    
+    // Start a single session for files comparison
     DiffSession session(argc, argv);
-
+    
+    // Get the diff and Display it for the running session
     session.showDiff();
 
     return 0;
