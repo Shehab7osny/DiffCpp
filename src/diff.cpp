c6c7a203c270da06ec5cdc2a908196fd033e1775
Shehab Hosny <shehab2015@live.com>
2021-01-13 00:04:58 +0200

    Update diff.cpp

--- src/diff.cpp
+++ src/diff.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(int argc, char** argv) {
 
     cout << Starting_MSG << endl;
-
+    
+    // Check for the minimum number of arguments required
     if (argc < 3) {
         cout << "Insufficient number of file specifications" << endl;
         return 0;
