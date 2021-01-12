d580f9160a2916c1db78962bd3e8bc48da8a94f7
Shehab Hosny <shehab2015@live.com>
2021-01-12 15:28:06 +0200

    Test Case 3 Added

--- test/DiffCppTest.cpp
+++ test/DiffCppTest.cpp
@@ -90,3 +90,34 @@ TEST(TestCase2, ComparePythonCode) {
 
     EXPECT_TRUE(true);
 }
+
+TEST(TestCase3, CompareCppCode) {
+    char* argvArray[4];
+    char** argv = argvArray;
+    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/TestCases/TestCase3/Input1.txt";
+    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/TestCases/TestCase3/Input2.txt";
+    argvArray[3] = (char*)"/W";
+
+    string lineToCheck;
+
+    DiffSession session(4, argv);
+
+    vector<string> linesFile1 = session.getLinesFromFile(argv[1]),
+        linesFile2 = session.getLinesFromFile(argv[2]);
+
+    vector<string> expectedOutput =
+        loadExpectedResults("/home/runner/work/DiffCpp/DiffCpp/test/TestCases/TestCase3/ExpectedResults.txt");
+
+    vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
+
+    if (expectedOutput.size() != diff.size()) {
+        EXPECT_TRUE(false);
+    }
+
+    for (int i = 0; i < diff.size(); i++) {
+        lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
+        EXPECT_EQ(lineToCheck, expectedOutput[i]);
+    }
+
+    EXPECT_TRUE(true);
+}
---
+++ test/TestCases/TestCase3/Input1.txt
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number, squareRoot;
+
+    number = 25.0;
+
+    // sqrt() is a library function to calculate the square root
+    squareRoot = sqrt(number);
+
+    cout << "Square root of " << number << " = " << squareRoot;
+
+    return 0;
+}
