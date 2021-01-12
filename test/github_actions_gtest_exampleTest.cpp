81c5ea712dc011ff9c9c683fad2cd4bdc0c9f24a
Shehab Hosny <shehab2015@live.com>
2021-01-12 02:58:47 +0200

    Update github_actions_gtest_exampleTest.cpp

--- test/github_actions_gtest_exampleTest.cpp
+++ test/github_actions_gtest_exampleTest.cpp
@@ -1,15 +1,5 @@
-#include <github_actions_gtest_example/github_actions_gtest_example.h>
-
 #include <gtest/gtest.h>
 
 TEST(githubActionsGtestExampleTest, firstTest) {
   EXPECT_TRUE( 1 == 1 );
-}
-
-TEST(githubActionsGtestExampleTest, addTest) {
-  const int a = 1;
-  const int b = 3;
-  const int result = githubActionsGtestExample::add(a, b);
-  
-  EXPECT_EQ( result, a + b );
 }
