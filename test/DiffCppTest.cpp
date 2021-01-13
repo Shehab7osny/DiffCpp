9ffb50ba12634b91b339d97d7570e0ab078db4d6
Shehab Hosny <shehab2015@live.com>
2021-01-13 17:28:49 +0200

    Added Unit Test 1

--- test/DiffCppTest.cpp
+++ test/DiffCppTest.cpp
@@ -31,31 +31,35 @@ vector<string> loadExpectedResults(string path)
     return lines;
 }
 
-TEST(BlockTest1, CheckingSimpleDiff) {
-    char* argvArray[3];
-    char** argv = argvArray;
-    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase1/Input1.txt";
-    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase1/Input2.txt";
-    
+TEST(BlockTest1, CheckingSimpleDiff)
+{
+    char *argvArray[3];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase1/Input1.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase1/Input2.txt";
+
     string lineToCheck;
 
     DiffSession session(3, argv);
-    
+
     vector<string> linesFile1 = session.getLinesFromFile(argv[1]),
                    linesFile2 = session.getLinesFromFile(argv[2]);
 
-    vector<string> expectedOutput = 
+    vector<string> expectedOutput =
         loadExpectedResults("/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase1/ExpectedResults.txt");
 
     vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
-    
-    if (expectedOutput.size() != diff.size()) {
+
+    if (expectedOutput.size() != diff.size())
+    {
         cout << "Unequal size of commits list" << endl;
         EXPECT_TRUE(false);
     }
 
-    else {
-        for (int i = 0; i < diff.size(); i++) {
+    else
+    {
+        for (int i = 0; i < diff.size(); i++)
+        {
             lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
             EXPECT_EQ(lineToCheck, expectedOutput[i]);
         }
@@ -64,11 +68,12 @@ TEST(BlockTest1, CheckingSimpleDiff) {
     }
 }
 
-TEST(BlockTest2, ComparePythonCode) {
-    char* argvArray[3];
-    char** argv = argvArray;
-    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase2/Input1.txt";
-    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase2/Input2.txt";
+TEST(BlockTest2, ComparePythonCode)
+{
+    char *argvArray[3];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase2/Input1.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase2/Input2.txt";
 
     string lineToCheck;
 
@@ -82,13 +87,16 @@ TEST(BlockTest2, ComparePythonCode) {
 
     vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
 
-    if (expectedOutput.size() != diff.size()) {
+    if (expectedOutput.size() != diff.size())
+    {
         cout << "Unequal size of commits list" << endl;
         EXPECT_TRUE(false);
     }
 
-    else {
-        for (int i = 0; i < diff.size(); i++) {
+    else
+    {
+        for (int i = 0; i < diff.size(); i++)
+        {
             lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
             EXPECT_EQ(lineToCheck, expectedOutput[i]);
         }
@@ -97,12 +105,13 @@ TEST(BlockTest2, ComparePythonCode) {
     }
 }
 
-TEST(BlockTest3, CompareCppCode) {
-    char* argvArray[4];
-    char** argv = argvArray;
-    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase3/Input1.txt";
-    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase3/Input2.txt";
-    argvArray[3] = (char*)"/W";
+TEST(BlockTest3, CompareCppCode)
+{
+    char *argvArray[4];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase3/Input1.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase3/Input2.txt";
+    argvArray[3] = (char *)"/W";
 
     string lineToCheck;
 
@@ -116,13 +125,16 @@ TEST(BlockTest3, CompareCppCode) {
 
     vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
 
-    if (expectedOutput.size() != diff.size()) {
+    if (expectedOutput.size() != diff.size())
+    {
         cout << "Unequal size of commits list" << endl;
         EXPECT_TRUE(false);
     }
 
-    else {
-        for (int i = 0; i < diff.size(); i++) {
+    else
+    {
+        for (int i = 0; i < diff.size(); i++)
+        {
             lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
             EXPECT_EQ(lineToCheck, expectedOutput[i]);
         }
@@ -131,11 +143,12 @@ TEST(BlockTest3, CompareCppCode) {
     }
 }
 
-TEST(BlockTest4, CheckingComplexDiff) {
-    char* argvArray[3];
-    char** argv = argvArray;
-    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase4/Input1.txt";
-    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase4/Input2.txt";
+TEST(BlockTest4, CheckingComplexDiff)
+{
+    char *argvArray[3];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase4/Input1.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase4/Input2.txt";
 
     string lineToCheck;
 
@@ -149,13 +162,16 @@ TEST(BlockTest4, CheckingComplexDiff) {
 
     vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
 
-    if (expectedOutput.size() != diff.size()) {
+    if (expectedOutput.size() != diff.size())
+    {
         cout << "Unequal size of commits list" << endl;
         EXPECT_TRUE(false);
     }
 
-    else {
-        for (int i = 0; i < diff.size(); i++) {
+    else
+    {
+        for (int i = 0; i < diff.size(); i++)
+        {
             lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
             EXPECT_EQ(lineToCheck, expectedOutput[i]);
         }
@@ -163,34 +179,74 @@ TEST(BlockTest4, CheckingComplexDiff) {
     }
 }
 
-TEST(BlockTest5, CheckingJavaCode) {
-    char* argvArray[4];
-    char** argv = argvArray;
-    argvArray[1] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase5/Input1.txt";
-    argvArray[2] = (char*)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase5/Input2.txt";
-    argvArray[3] = (char*)"/W";
+TEST(BlockTest5, CheckingJavaCode)
+{
+    char *argvArray[4];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase5/Input1.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase5/Input2.txt";
+    argvArray[3] = (char *)"/W";
 
     string lineToCheck;
 
     DiffSession session(4, argv);
 
     vector<string> linesFile1 = session.getLinesFromFile(argv[1]),
-        linesFile2 = session.getLinesFromFile(argv[2]);
+                   linesFile2 = session.getLinesFromFile(argv[2]);
 
     vector<string> expectedOutput =
         loadExpectedResults("/home/runner/work/DiffCpp/DiffCpp/test/BlockTests/TestCase5/ExpectedResults.txt");
 
     vector<vector<string>> diff = session.getDiff(linesFile1, linesFile2);
 
-    if (expectedOutput.size() != diff.size()) {
+    if (expectedOutput.size() != diff.size())
+    {
         cout << "Unequal size of commits list" << endl;
         EXPECT_TRUE(false);
     }
 
-    else {
-        for (int i = 0; i < diff.size(); i++) {
+    else
+    {
+        for (int i = 0; i < diff.size(); i++)
+        {
             lineToCheck = "[" + diff[i][0] + "]" + diff[i][1];
             EXPECT_EQ(lineToCheck, expectedOutput[i]);
+        }
+
+        EXPECT_TRUE(true);
+    }
+}
+
+TEST(UnitTest1, CheckingLoadingLines)
+{
+    char *argvArray[3];
+    char **argv = argvArray;
+    argvArray[1] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/UnitTests/TestCase1/Input.txt";
+    argvArray[2] = (char *)"/home/runner/work/DiffCpp/DiffCpp/test/UnitTests/TestCase1/Input.txt";
+
+    DiffSession session(3, argv);
+
+    vector<string> linesFile = session.getLinesFromFile(argv[1]);
+
+    vector<string> expectedOutput =
+        loadExpectedResults("/home/runner/work/DiffCpp/DiffCpp/test/UnitTests/TestCase1/ExpectedResults.txt");
+
+    for (int i = 0; i < linesFile.size(); i++)
+    {
+        cout << linesFile[i] << endl;
+    }
+
+    if (expectedOutput.size() != linesFile.size())
+    {
+        cout << "Unequal size of commits list" << endl;
+        EXPECT_TRUE(false);
+    }
+
+    else
+    {
+        for (int i = 0; i < linesFile.size(); i++)
+        {
+            EXPECT_EQ(linesFile[i], expectedOutput[i]);
         }
 
         EXPECT_TRUE(true);
