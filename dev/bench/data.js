db3f3d806a3b7d406cb7a3659804c9130e891152
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 17:15:26 +0000

    add Benchmark (googlecpp) benchmark result for c591b1012244f0ead657982c4366553b3c5dcfb3

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637774126506,
+  "repoUrl": "https://github.com/Shehab7osny/DiffCpp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "shehab2015@live.com",
+            "name": "Shehab Hosny",
+            "username": "Shehab7osny"
+          },
+          "committer": {
+            "email": "shehab2015@live.com",
+            "name": "Shehab Hosny",
+            "username": "Shehab7osny"
+          },
+          "distinct": true,
+          "id": "c591b1012244f0ead657982c4366553b3c5dcfb3",
+          "message": "commit new performance check",
+          "timestamp": "2021-11-24T19:04:33+02:00",
+          "tree_id": "339ee86fa62459455df3a2dd43c3fe86284c3aaf",
+          "url": "https://github.com/Shehab7osny/DiffCpp/commit/c591b1012244f0ead657982c4366553b3c5dcfb3"
+        },
+        "date": 1637774126014,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "DiffCpp_Benchmark",
+            "value": 2368747.4576271116,
+            "unit": "ns/iter",
+            "extra": "iterations: 295\ncpu: 2368090.915254237 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
